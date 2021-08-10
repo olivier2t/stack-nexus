0a5b156ae3971dc284bc4a0789e25ef1c18786fb
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-10 10:27:35 +0200

    adding a default region in tf for Cost Estimation

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -6,7 +6,10 @@ variable "customer" {}
 # AWS variables
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "aws_region" {
+  description = "AWS region where to create servers."
+  default     = "eu-west-1"
+}
 variable "terraform_storage_bucket_name" {}
 variable "keypair_public" {}
 
