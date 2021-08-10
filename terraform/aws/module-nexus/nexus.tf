94fb0c6847c4f84cdf820a19957ebefc071c08ee
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-10 05:49:01 +0200

    first version

--- terraform/aws/module-nexus/nexus.tf
+++ terraform/aws/module-nexus/nexus.tf
@@ -4,7 +4,14 @@ resource "aws_security_group" "nexus" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -3,17 +3,17 @@
 #
 output "vpc_id" {
   description = "Amazon VPC ID where the Nexus Repository is created."
-  value       = data.terraform_remote_state.outputs.infra.vpc_id
+  value       = data.terraform_remote_state.infra.outputs.vpc_id
 }
 
 output "public_subnets" {
   description = "The public subnets for the VPC"
-  value       = data.terraform_remote_state.outputs.public_subnets
+  value       = data.terraform_remote_state.infra.outputs.public_subnets
 }
 
 output "bastion_ip" {
   description = "The IP address of the bastion."
-  value       = data.terraform_remote_state.outputs.infra.bastion_ip
+  value       = data.terraform_remote_state.infra.outputs.infra.bastion_ip
 }
 
 #
